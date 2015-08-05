d7f3fcd8f251f985dd3e6407a9f36a759d9a9d51
Ziggy Lotivio <dook@stardust.red>
2015-08-05 02:09:24 -0400

    Fixed #10, timer not displaying on start, and set up fix for #11

--- index.js
+++ index.js
@@ -88,6 +88,12 @@ Timer.prototype.lap = function() {
 }
 
 Timer.prototype.draw  = function(reset) {
+  if(reset) {
+    // Timer should be redrawn to 00:00.00
+    this.dom_timer.innerHTML     = "00:00.00";
+    this.dom_timer_lap.innerHTML = "00:00.00";
+  }
+
   if(this.state == 1) { // active
     // Convert this.start_time to string
     var timer     = this.convert(new Date(this.elapsed_time));
